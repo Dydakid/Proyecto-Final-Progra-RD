cfd00772ed969730ae085d67b6e80cce13c4eadd
Dydakid <Dydaswagg7@mesoamericana.edu.gt>
2017-05-22 23:44:21 -0600

    Se agregaron 3 Clases, Comunidad, Gastos y Propietarios

---
+++ Proyecto Final RD/Proyecto Final RD/Comunidad.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_RD
+{
+    class Comunidad
+    {
+        int indetificacion;
+        string nombre;
+        string poblacion;
+
+        public int Indetificacion
+        {
+            get
+            {
+                return indetificacion;
+            }
+
+            set
+            {
+                indetificacion = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Poblacion
+        {
+            get
+            {
+                return poblacion;
+            }
+
+            set
+            {
+                poblacion = value;
+            }
+        }
+    }
+}
---
+++ Proyecto Final RD/Proyecto Final RD/Gastos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_RD
+{
+    class Gastos
+    {
+        string identificacion;
+        string nombre;
+        string tipoDeReparto;
+
+        public string Identificacion
+        {
+            get
+            {
+                return identificacion;
+            }
+
+            set
+            {
+                identificacion = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string TipoDeReparto
+        {
+            get
+            {
+                return tipoDeReparto;
+            }
+
+            set
+            {
+                tipoDeReparto = value;
+            }
+        }
+    }
+}
